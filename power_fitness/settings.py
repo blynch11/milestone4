4f7f7cdd59f5fb7c6831bdb83321e69069836916
blynch11 <blynch2491@gmail.com>
2021-11-15 19:06:25 +0000

    added home app & templates

--- power_fitness/settings.py
+++ power_fitness/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for power_fitness project.
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +60,10 @@ ROOT_URLCONF = 'power_fitness.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
