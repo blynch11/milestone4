83025b6feefbdba4b5eddb6fd2b870fc8f62d04c
blynch11 <blynch2491@gmail.com>
2021-11-22 18:24:16 +0000

    added home page card and changed styling. Added prodcut detail page - needs work to be completed, added new fonts

--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import product
 
 # Create your views here.
 
 def all_products(request):
+    """ all products view """
 
     products = product.objects.all()
 
@@ -12,3 +13,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ individual products view """
+    
+    product = get_object_or_404(product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
