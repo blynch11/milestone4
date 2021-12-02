93b4cd20c7513e855c226ed2cb08599a9cfedf96
blynch11 <blynch2491@gmail.com>
2021-12-02 20:19:17 +0000

    added basket functionality

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -7,17 +9,32 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0 
+    basket = request.session.get('basket', [])
 
-        
-    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+      
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+    
+    grand_total = delivery + total
 
     context = {
         'basket_items': basket_items,
         'total': total, 
         'product_count': product_count,
         'delivery': delivery,
-        
-        
     }
 
     return context
