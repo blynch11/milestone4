f8f1923603599c8309733f3b19a7bb0d7b117ca9
blynch11 <blynch2491@gmail.com>
2021-12-14 18:41:30 +0000

    prepared for deployment

--- power_fitness/settings.py
+++ power_fitness/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-8o%3v82c0p$ulb^yi4((1exqj6bwerv(zn^qqtoa-b!3k*5kf7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'blynch11-milestone-4-ci.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -104,12 +104,17 @@ WSGI_APPLICATION = 'power_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
